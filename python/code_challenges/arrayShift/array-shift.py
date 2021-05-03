b3d7b4bc64030289b233b4189a5414c8d0d9f7ed
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-05-03 23:01:36 +0300

    the challenge 2 finished without tests yet

---
+++ python/code_challenges/arrayShift/array-shift.py
@@ -0,0 +1,16 @@
+def findLength(list):
+  count = 0
+  for i in list:
+    count+= 1
+  return count
+
+def insertShiftArray(list ,length,value):
+  if length%2!=0:
+     length+=1
+  index=int(length/2)
+  list[index:index]=[value]
+  return list
+
+
+print(insertShiftArray([2,4,6,8],findLength([2,4,6,8]),5))
+print(insertShiftArray([4,8,15,23,42],findLength([4,8,15,23,42]),16))
