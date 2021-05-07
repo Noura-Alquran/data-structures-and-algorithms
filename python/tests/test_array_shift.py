267089edeae2ec642318658ddf3d8a67b62c6c63
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-05-07 22:51:06 +0300

    test code-challenge 2

---
+++ python/tests/test_array_shift.py
@@ -0,0 +1,9 @@
+from code_challenges.arrayShift.array_shift import findLength ,insertShiftArray
+
+def test_insert_shift_Array(test_list=[1,2,3,5,6] ,value=4):
+    expected=[1,2,3,4,5,6]
+    actual=insertShiftArray(test_list,len(test_list),value)
+    assert expected==actual
+
+
+
