629d0b984eccbf5a6c37513e89d201b145d50e12
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-05-19 00:36:07 +0300

    codeChallenge8

--- python/tests/test_zip_lists.py
+++ python/tests/test_zip_lists.py
@@ -2,10 +2,18 @@ from Data_Structures.linked_list.linked_list import LinkedList
 from code_challenges.zipLists.zip_lists import zipLists
 import pytest
 
+# Happy path test
 def test_zip_lists(zipLists_test):
     expected="{25} -> {5} -> {1996} -> {my} -> {birthday} ->  None"
     actual=f"{zipLists_test}"
     assert actual==expected
+
+# when one of the lists empty
+def test_zip_list_two(edge_test):
+    expected="{25} -> {1996} -> {birthday} ->  None"
+    actual=f"{edge_test}"
+    assert actual==expected
+
 
 
 @pytest.fixture
@@ -20,3 +28,12 @@ def zipLists_test():
     result=zipLists(linked_list_one,linked_list_two)
     return result
 
+@pytest.fixture
+def edge_test():
+    linked_list_one= LinkedList()
+    linked_list_two= LinkedList()
+    linked_list_one.append(25)
+    linked_list_one.append(1996)
+    linked_list_one.append("birthday")
+    result=zipLists(linked_list_one,linked_list_two)
+    return result
