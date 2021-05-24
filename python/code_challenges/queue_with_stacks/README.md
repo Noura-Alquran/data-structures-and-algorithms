6d906212731635182d465dbec684e0005bde9616
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-05-24 03:04:16 +0300

    queue with stacks challenge

---
+++ python/code_challenges/queue_with_stacks/README.md
@@ -0,0 +1,28 @@
+# Code Challenge 11 :
+## Implement queue from two stacks
+
+## Challenge
+* Create a brand new PseudoQueue class. Do not use an existing Queue. Instead, this PseudoQueue class will implement our standard queue interface (the two methods listed below), but will internally only utilize 2 Stack objects. Ensure that you create your class with the following methods:
+ 1. enqueue(value) which inserts value into the PseudoQueue, using a first-in, first-out approach.
+ 2. dequeue() which extracts a value from the PseudoQueue, using a first-in, first-out approach.
+* The Stack instances have only push, pop, and peek methods. You should use your own Stack implementation. Instantiate these Stack objects in your PseudoQueue constructor.
+
+## Approach & Efficiency:
+### Whiteboard:
+![image]()
+* Big O :
+  - enqueue() >> O(1) .
+  - dequeue >> O(n) .
+
+# Tasks:
+- [x] Top-level README “Table of Contents” is updated
+- [x] Feature tasks for this challenge are completed
+- [x] Unit tests written and passing
+- [x] “Happy Path” - Expected outcome
+- [x] Expected failure
+- [x] Edge Case (if applicable/obvious)
+- [x] README for this challenge is complete
+- [x] Description, Approach & Efficiency, Solution
+- [x] Link to code
+
+* [code](queue_with_stacks.py)
--- python/code_challenges/queue_with_stacks/queue_with_stacks.py
+++ python/code_challenges/queue_with_stacks/queue_with_stacks.py
@@ -16,7 +16,6 @@ class Node():
 class Stack():
     def __init__(self,node=None):
         self.top=node
-        self._items = []
 
     def push(self, value):
         if self.top == None:
@@ -52,8 +51,7 @@ class Stack():
             items.append(str(top.data))
             top = top.next
         return " ".join(items)
-    def __len__(self):
-        return len(self._items)
+
 
 class PseudoQueue():
     def __init__(self):
@@ -65,28 +63,40 @@ class PseudoQueue():
     def enqueue(self, value):
         self.stack_1.push(value)
 
-
-    def dequeue(self):
+    def dequeue(self ):
         if self.stack_2.top == None:
             if self.stack_1.top== None:
                 return "Can't dequeue from empty queue!"
-
-        while self.stack_1.top:
-            last_stack_1_item = self.stack_1.pop()
-            self.stack_2.push(last_stack_1_item)
-        return self.stack_2.pop()
-
+        while not self.stack_1.isEmpty():
+            popped = self.stack_1.pop()
+            self.stack_2.push(popped)
+        result = self.stack_2.pop()
+        if self.stack_1.isEmpty():
+            while not self.stack_2.isEmpty():
+                 self.stack_1.push(self.stack_2.pop())
+            return result
 
     def __str__(self):
-         return str(self.stack_1)
+        stack= self.stack_1.top
+        items = []
+        while stack:
+            items.insert(0,str(stack.data))
+            stack = stack.next
+        return " ".join(items)
 
-if __name__=="__main__":
+
+if __name__== "__main__" :
     pesudoQ= PseudoQueue()
     pesudoQ.enqueue(1)
     pesudoQ.enqueue(2)
     pesudoQ.enqueue(3)
     pesudoQ.enqueue(4)
     print(pesudoQ)
-    pesudoQ.dequeue()
-    pesudoQ.dequeue()
+    print(pesudoQ.dequeue())
     print(pesudoQ)
+
+
+
+
+
+
